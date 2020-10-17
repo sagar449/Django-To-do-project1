37685fbdd2250d5dd1992fa7ff121d3dc029cf7b
sagar449 <sagarchaudhari.compsci@gmail.com>
2020-10-17 22:59:55 +0530

    My very first commit

---
+++ myapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Student(models.Model):
+    sid = models.CharField(max_length=20)
+    sname = models.CharField(max_length=100)
+    semail = models.EmailField()
+    scontact = models.CharField(max_length=30)
